e2ec1bfd21f18cc0fbd113086dbc724c5b5ebfdb
Turobov.xc <154534597+OzodbekTurobov2008@users.noreply.github.com>
2024-03-26 14:24:11 +0500

    Add files via upload

---
+++ orqa_fon/remove_bg.py
@@ -0,0 +1,26 @@
+import requests
+
+
+def removebg(FILE_NAME):
+    rasm=''
+    API_KEY ='54y1DvCcPDMXk61mNMSEu4JB'
+
+
+    response = requests.post(
+    'https://api.remove.bg/v1.0/removebg',
+    data={
+        'image_url': FILE_NAME,
+        'size': 'auto',
+        #'bg_image_url':'image/url'
+    },
+    files = {
+        #'bg_image_file':open("background.jpg","rb"),
+    },
+    headers={'X-Api-Key': API_KEY},
+)
+    if response.status_code == requests.codes.ok:
+      
+        rasm = response.content
+    else:
+        print("Error:", response.status_code, response.text)
+    return rasm
